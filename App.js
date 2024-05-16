531da049e8296ea944b2ff9f8305fccc0d32db4f
Amna1993 <amnarazzaqawan@gmail.com>
2024-05-16 01:11:03 -0400

    startup code

---
+++ App.js
@@ -0,0 +1,15 @@
+
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "h1 Tag"),
+        React.createElement("h2", {}, "h2 Tag"),
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "h1 Tag"),
+        React.createElement("h2", {}, "h2 Tag"),
+    ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
